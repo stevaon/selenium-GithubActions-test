a791e41c42578b0237df236b2de164efec1de35f
lttlerZer <2401997075@qq.com>
2022-03-21 09:56:47 +0800

    w

---
+++ main.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import time
+
+position = dict({
+        "latitude":  34.226692,
+        "longitude": 108.954232,
+        "accuracy": 100
+        })
+
+chrome_option = Options()
+
+chrome_option.add_argument('--headless')
+chrome_option.add_argument('--no-sandbox')
+chrome_option.add_argument('window-size=1920x1080') # 指定浏览器分辨率
+chrome_option.add_argument('--disable-gpu')
+chrome_option.add_experimental_option('excludeSwitches', ['enable-automation'])
+# action端
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_option)
+
+driver.get("https://www.baidu.com/")
+# 伪装地址
+driver.command_executor._commands['set_permission'] = (
+'POST', '/session/$sessionId/permissions')
+print("driver.command_executor._commands is successful")
+driver.execute(
+    'set_permission',
+    {
+        'descriptor': { 'name': 'geolocation' },
+        'state': 'granted'
+    }
+)
+
+print("driver.execute is successful")
+driver.execute_cdp_cmd(
+    'Emulation.setGeolocationOverride', {
+    "latitude": position['latitude'],
+    "longitude": position['longitude'],
+    "accuracy": position['accuracy']
+})
+
+print("driver.execute_cdp_cmd is successful")
+time.sleep(2)
+
+print("test ok")
