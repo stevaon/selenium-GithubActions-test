9e6986b875e806318a7036d9bafdbd28f14b14c2
stevaon <46243429+stevaon@users.noreply.github.com>
2022-03-21 11:09:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import os
 
 env_dist = os.environ
 position = dict({
-        "latitude":  env_dist['latitude'],
-        "longitude": env_dist['longitude'],
+        "latitude":  float(env_dist['latitude']),
+        "longitude": float(env_dist['longitude']),
         "accuracy": 100
         })
         
